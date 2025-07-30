f6b89a8155c9b8abe90ac252a97012400555ef11
ericbowser <ericryanbowser@gmail.com>
2025-07-29 19:57:25 -0600

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig } from 'vite';
+import fs from "fs"; // Import the file system module
 
 export default defineConfig({
+    server: {
+        https: {
+            key: fs.readFileSync('./ssl_cert/127.0.0.1+1-key.pem'), // Replace with actual path
+            cert: fs.readFileSync('./ssl_cert/127.0.0.1+1.pem'), // Replace with actual path
+        },
+    },
     css: {
         postcss: {
             plugins: [
